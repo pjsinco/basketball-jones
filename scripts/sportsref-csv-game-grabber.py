6bfacc5e1b57eb0cb2fb854ebe193d2d5ca9f6ea
pjsinco <psinco@fas.harvard.edu>
2014-03-26 11:32:11 -0500

    added scripts that starts to scrape sportsref game csv's

---
+++ scripts/sportsref-csv-game-grabber.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+#work in progress
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+#open firefox
+driver = webdriver.Firefox()
+
+#open a url
+driver.get('http://www.sports-reference.com/cbb/boxscores/2012-12-29-georgia-tech.html')
+
+#find the element we want -- the 'CSV' link
+csv = driver.find_element_by_css_selector("span[tip$='comma-separated values']")
+
+#click it
+csv.click()
+
+#scrape it
+
+#save it to a file
