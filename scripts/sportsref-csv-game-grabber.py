9099d004a69b0ea5a59642161bb61764c582e3d4
pjsinco <psinco@fas.harvard.edu>
2014-03-26 18:45:36 -0500

    iterated on sportsref-csv_game_grabber.py, grabbed a sample batch of games for ga-tech

--- scripts/sportsref-csv-game-grabber.py
+++ scripts/sportsref-csv-game-grabber.py
@@ -1,33 +1,67 @@
 #!/usr/bin/env python
 
 #selenium webdriver api
-#https://selenium-python.readthedocs.org/en/latest/api.html
+#http://selenium-python.readthedocs.org/getting-started.html
 
 ####################
 # work in progress #
 ####################
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-#from bs4 import BeautifulSoup
+from datetime import date, datetime, timedelta
+import time 
+
+#http://stackoverflow.com/questions/10688006/
+#  generate-a-list-of-datetimes-between-an-interval-in-python
+def perdelta(start, end, delta):
+  curr = start
+  while curr < end:
+    yield curr
+    curr += delta
+
+#rough season start and end dates
+startdate = date(2012, 11, 01)
+enddate = date(2013, 04, 15)
+
+team = 'georgia-tech'
 
 #open firefox
 driver = webdriver.Firefox()
 
-#open a url
-driver.get('http://www.sports-reference.com/cbb/boxscores/2012-12-29-georgia-tech.html')
+for d in perdelta(startdate, enddate, timedelta(days = 1)):
 
-#find the element we want -- the 'CSV' link
-csv = driver.find_element_by_css_selector("span[tip$='comma-separated values']")
+  #be a good citizen
+  time.sleep(2)
 
-#click it
-csv.click()
+  #open a url
+  driver.get('%s%s-%s.html' % \
+    ('http://www.sports-reference.com/cbb/boxscores/', d, team))
+  
+  #if we hit upon a game on that date, scrape it
+  if not (driver.title.startswith('File Not Found')):
 
-#scrape it
-pre = driver.find_element_by_tag_name('pre')
-text = pre.text
+    #find the element we want -- the 'CSV' link
+    elem = driver.find_element_by_xpath('//span[contains(@onclick, \
+      "table2csv(\'%s")]' % (team))
 
-#for now, show that we grabbed it
-print (text)
+    #click it
+    elem.click()
 
-#save scraped text to a file
+    #scrape it
+    pre = driver.find_element_by_tag_name('pre')
+    text = pre.text
+
+    #save scraped data to a file
+    file = open('../data/%s-%s.csv' % (d, team), 'w')
+    file.write(text)
+    file.close()
+
+#close driver
+driver.close()
+
+
+"""
+NOTES
+
+
+"""
