160ab7a2ba94f05aed9c5328048e3319fbbf8cfe
Patrick Sinco <psinco@gmail.com>
2014-03-29 02:40:35 -0500

    made minor changes to espn-gameplay-scraper.js

--- scripts/espn-gameplay-scraper.js
+++ scripts/espn-gameplay-scraper.js
@@ -5,19 +5,21 @@
   var play = null;
   
   $.ajax({
-    url: '../scraps/espn-dayton-game.html',
+    //url: '../scraps/espn-dayton-game.html',
+    //url: 'http://espn.go.com/ncb/playbyplay?gameId=400504622',
+    url: 'http://espn.go.com/ncb/playbyplay?gameId=323152168',
     type: 'GET',
-    success: function(request) {
+    success: function(data, textStatus) {
+      console.log(data);
       // grab all the rows
-      var rows = $(request).find('.mod-data.mod-pbp tbody tr');
+      var rows = $(data.responseText).find('.mod-data.mod-pbp tbody tr');
       var numRows = rows.length;
-  
       // grab all the TDs
       var tds = $(rows).find('td');
       
       // dynamically establish number of TDs in each row
       var numTDsInRow = $(rows[0]).find('td').length;
-  
+
       // grab all the TDs in each row, and process them
       rows.each(function(i) {
         plays[i] = {}; // set up a new play object ...
@@ -26,6 +28,7 @@
         // process each TD
         var tds = $(this).find('td');
         tds.each(function(i) {
+          console.log(i);
           if (i % numTDsInRow === 0) { 
             // we're in the first TD;
             // if there are no children; children in a TD indicated
@@ -33,7 +36,7 @@
             // i think there always is a clock TD, but just in case
             // let's check
             if ($(this).children().length < 1) { 
-              play['clock'] = $(this).text();
+              play['clock'] = $(this).text().trim()
             } else {
               play['clock'] = '';
             }
@@ -41,6 +44,7 @@
             // we're in the second TD;
             // make sure there are no children, which would
             // indicate something unusual
+            console.log($(this));
             if ($(this).children().length < 1) {
               play['away_play'] = $(this).text();
             } else {
