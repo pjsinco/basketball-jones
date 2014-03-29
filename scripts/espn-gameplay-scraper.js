c4be7087d7bb1c151947b548a10d5582dbf70bd0
Patrick Sinco <psinco@gmail.com>
2014-03-29 03:45:52 -0500

    refined espn-gameplay-scraper.html

--- scripts/espn-gameplay-scraper.js
+++ scripts/espn-gameplay-scraper.js
@@ -1,71 +1,80 @@
-(function() {
+$(document).ready(function() {
   console.log('flyer');
 
   var plays = []; // hold all our play objects
   var play = null;
+
+  var saveToFile = function(arrayOfLines, fileName) {
+    /* adds linebreaks at the end*/
+    var blob, blobText;
+    blobText = arrayOfLines.map(function(d) {
+      if (d.endsWith("\n")) {
+        return d;
+      } else {
+        return d + "\n";
+      }
+    });
+    blob = new Blob(blobText, {
+      type: "text/plain;charset=utf-8"
+    });
+    return saveAs(blob, fileName);
+  };
+
   
   $.ajax({
-    //url: '../scraps/espn-dayton-game.html',
-    //url: 'http://espn.go.com/ncb/playbyplay?gameId=400504622',
     url: 'http://espn.go.com/ncb/playbyplay?gameId=323152168',
     type: 'GET',
     success: function(data, textStatus) {
-      console.log(data);
+
       // grab all the rows
       var rows = $(data.responseText).find('.mod-data.mod-pbp tbody tr');
       var numRows = rows.length;
+
       // grab all the TDs
       var tds = $(rows).find('td');
       
-      // dynamically establish number of TDs in each row
+      // dynamically establish baseline number of TDs in each row
       var numTDsInRow = $(rows[0]).find('td').length;
 
       // grab all the TDs in each row, and process them
       rows.each(function(i) {
-        plays[i] = {}; // set up a new play object ...
-        play = plays[i]; // ... and put it in our array
+        plays[i] = {}; // initialize a new object in our plays array ...
+        play = plays[i]; // ... and grab hold of it
 
         // process each TD
         var tds = $(this).find('td');
-        tds.each(function(i) {
-          console.log(i);
-          if (i % numTDsInRow === 0) { 
+        tds.each(function(k) {
+          if (k % numTDsInRow === 0) { 
             // we're in the first TD;
-            // if there are no children; children in a TD indicated
-            // something unusual, like a timeout or end of half;
-            // i think there always is a clock TD, but just in case
-            // let's check
-            if ($(this).children().length < 1) { 
               play['clock'] = $(this).text().trim()
-            } else {
-              play['clock'] = '';
-            }
-          } else if (i % numTDsInRow === 1) { 
+          } else if (k % numTDsInRow === 1) { 
             // we're in the second TD;
-            // make sure there are no children, which would
-            // indicate something unusual
-            console.log($(this));
-            if ($(this).children().length < 1) {
-              play['away_play'] = $(this).text();
+            // check that there are more than 2 TDs in this row;
+            // if there aren't, something is unusual about this play,
+            // like a timeout or end of half, 
+            // in which case we'll only note the clock
+            if (tds.length > 2) {
+              play['away_play'] = $(this).text().trim();
             } else {
               play['away_play'] = '';
               play['score'] = '';
               play['home_play'] = '';
             }
-          } else if (i % numTDsInRow === 2) {
+          } else if (k % numTDsInRow === 2) {
             // we're in the third TD;
-            // make sure there are no children, which would
-            if ($(this).children().length < 1) {
-              play['score'] = $(this).text();
+            // we probably don't need to check number of TDs at this
+            // point, but we will just to be safe
+            if (tds.length > 2) {
+              play['score'] = $(this).text().trim();
             } else {
               play['score'] = '';
               play['home_play'] = '';
             }
-          } else if (i % numTDsInRow === 3) {
+          } else if (k % numTDsInRow === 3) {
             // we're in the third TD;
-            // make sure there are no children, which would
-            if ($(this).children().length < 1) {
-              play['home_play'] = $(this).text();
+            // again, check tds.length just to be safe
+            if (tds.length > 2) {
+              play['home_play'] = $(this).text().trim();
             } else {
               play['home_play'] = '';
             }
@@ -75,6 +84,7 @@
       });
       
       console.log(plays);
+      //saveToFile(plays, 'temp');
     } // end success
   }); // end $.ajax()
-})(); 
+}); 
