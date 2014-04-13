51c479d73f4acdbca77def968fdf4b3f38084774
Patrick Sinco <psinco@gmail.com>
2014-04-13 09:38:51 -0500

    puts box-scores into box-scores-all.json

--- scripts/dupes.js
+++ scripts/dupes.js
@@ -1,6 +1,6 @@
 console.log('hiya');
 
-d3.json('../data/box-scores.json', function(data) {
+d3.json('../data/box-scores-all.json', function(data) {
   d3.keys(data).forEach(function(d) {
     console.log(d);
   });
