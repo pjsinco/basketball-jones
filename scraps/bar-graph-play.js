36bea9f711326f7edc647cd5bdeb73b0247d1360
pjsinco <psinco@fas.harvard.edu>
2014-04-24 17:24:59 -0500

    begins to build bar-graph-play

---
+++ scraps/bar-graph-play.js
@@ -0,0 +1,12 @@
+console.log('blue demon');
+
+// time scales
+// http://jsfiddle.net/robdodson/KWRxW/
+// http://bl.ocks.org/mbostock/1849162
+// http://bl.ocks.org/mbostock/6186172
+// http://bl.ocks.org/mbostock/1166403
+// http://bl.ocks.org/phoebebright/3059392
+// http://stackoverflow.com/questions/20181286/
+//  how-to-use-d3-time-scale-to-generate-an-
+//    array-of-evenly-spaced-dates
+
