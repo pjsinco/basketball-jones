1b2f74277bbe1c8db244a3ce2bef4dc0396d9848
Patrick Sinco <psinco@gmail.com>
2014-04-25 21:46:04 -0500

    progresses on bar-graph-play

--- scraps/bar-graph-play.js
+++ scraps/bar-graph-play.js
@@ -2,11 +2,15 @@ console.log('blue demon');
 
 // time scales
 // http://jsfiddle.net/robdodson/KWRxW/
-// http://bl.ocks.org/mbostock/1849162
+// http://bl.ocks.org/mbostock/1849162 // yes
 // http://bl.ocks.org/mbostock/6186172
 // http://bl.ocks.org/mbostock/1166403
 // http://bl.ocks.org/phoebebright/3059392
-// http://stackoverflow.com/questions/20181286/how-to-use-d3-time-scale-to-generate-an-array-of-evenly-spaced-dates
+// http://stackoverflow.com/questions/20181286/
+//    how-to-use-d3-time-scale-to-generate-an-array-of-
+//      evenly-spaced-dates
+// http://stackoverflow.com/questions/10127402/
+//    bar-chart-with-negative-values // yes
 
 var margin = {
   top: 50,
@@ -22,13 +26,19 @@ var timeFormat = d3.time.format('%Y-%m-%d').parse;
 // ex.: timeFormat('2013-01-05');
 // returns a js Date object
 
-var xScale = d3.time.scale()
-  .range([0, width]);
+var xScale = d3.scale.ordinal()
+  //.range([0, width]);
+  .rangeRoundBands([0, width], .7, 0.5);
 
 var yScale = d3.scale.linear()
   .range([height, 0]);
 
-var svg = d3.select('body')
+var xAxis = d3.svg.axis()
+  .scale(xScale)
+  .orient('bottom')
+  //.tickSize(6, 6)
+
+var chart = d3.select('body')
   .append('svg')
   .attr('width', width + margin.left + margin.right)
   .attr('height', height + margin.top + margin.bottom)
@@ -37,8 +47,64 @@ var svg = d3.select('body')
     + margin.top + ')');
 
 
+getGames('2582', function(record, games) {
+  console.log(record);
+  console.log(games);
+
+  //var extent = d3.extent(games.map(function(d) {
+    //return timeFormat(d.details.date);
+  //}));
+
+  xScale
+    .domain([0, games.length]);
+
+  yScale
+    .domain(d3.extent(games.map(function(d) {
+      return d.details.home.pts - d.details.away.pts;
+    })));
+
+  console.log(yScale.domain());
+  
+  var marginExtent = games.map(function(d) {
+    //console.log(d);
+  })
+
+  xAxis
+    .tickValues(d3.range([1, games.length]))
+  chart
+    .append('g')
+    .attr('class', 'x axis')
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxis);
+
+  chart
+    .selectAll('bar')
+    .data(games)
+    .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', function(d, i) {
+        return xScale(i);
+      })
+      .attr('y', function(d) {
+        return yScale(d.details.home.pts - d.details.away.pts);
+      })
+      .attr('height', function(d) {
+        return height - yScale(d.details.home.pts
+          - d.details.away.pts);
+      })
+      .attr('width', xScale.rangeBand());
+
+  console.log(xScale.rangeBand());
+  
+
+});
+
+
+
+
+function getGames(team, callback) {
 // returns stats for all season's games for the given team
-function getGames(team) {
   d3.json('../data/box-scores-all.json', function(error, data) {
     
     // count wins, losses
@@ -46,19 +112,22 @@ function getGames(team) {
       wins: 0,
       losses: 0
     }
+  
+    var games = [];
 
     // find all games involving the team
     d3.keys(data).forEach(function(d) {
       if (data[d].details.away.id == team ||
         data[d].details.home.id == team) {
+          games.push(data[d])
           if (data[d].details.winner == team) {
             record.wins++;
           } else {
             record.losses++;
           }
         }
-      })
+      });
 
-    return record;
+    callback(record, games);
   }); // end d3.json()
 }
