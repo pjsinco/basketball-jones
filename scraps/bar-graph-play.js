e676d0091fbf719c237e163cb2a76cffa2da7e02
Patrick Sinco <psinco@gmail.com>
2014-04-26 09:34:36 -0500

    gets negative and positive values appearing correctly on bar graph

--- scraps/bar-graph-play.js
+++ scraps/bar-graph-play.js
@@ -1,32 +1,35 @@
-console.log('blue demon');
-
-// bar chart with negative values
+// help from bar chart with negative values
 // http://bl.ocks.org/mbostock/2368837
 
 var margin = {
-  top: 50,
-  right: 50,
-  bottom: 50,
-  left: 50
+  top: 10,
+  right: 10,
+  bottom: 10,
+  left: 10
 }
 
-var width = 800 - margin.left - margin.right;
-var height = 200 - margin.top - margin.bottom;
+var width = 600 - margin.left - margin.right;
+var height = 70 - margin.top - margin.bottom;
 
 var timeFormat = d3.time.format('%Y-%m-%d').parse;
 // ex.: timeFormat('2013-01-05');
 // returns a js Date object
 
 var xScale = d3.scale.ordinal()
-  .rangeRoundBands([0, width], 0.05);
+  .rangeRoundBands([0, width], 0.15);
 
 var yScale = d3.scale.linear()
-  .range([height, 0]);
+  .range([0, height])
+  .nice();
 
-var xAxis = d3.svg.axis()
-  .scale(xScale)
-  .orient('bottom')
-  .tickSize(6, 6)
+//var xAxis = d3.svg.axis()
+//  .scale(xScale)
+//  .orient('bottom')
+//  .tickSize(6, 6)
+
+//var yAxis = d3.svg.axis()
+//  .scale(yScale)
+//  .orient('left')
 
 var chart = d3.select('body')
   .append('svg')
@@ -40,64 +43,73 @@ var teamHighlighted = '2582';
 
 getGames(teamHighlighted, function(games) {
 
-  //var extent = d3.extent(games.map(function(d) {
-    //return timeFormat(d.details.date);
-  //}));
-
   xScale
     .domain(d3.range(games.length));
 
   yScale
     .domain(d3.extent(games.map(function(d) {
-      return d.details.home.pts - d.details.away.pts;
+      return d.details.margin;
     })));
 
+  console.log(yScale.domain());
   console.log(games);
 
-  chart
-    .append('g')
-    .attr('class', 'x axis')
-    .attr('transform', 'translate(0,' + height + ')')
-    .call(xAxis);
 
-  chart
-    .selectAll('.x.axis .tick')
-    .text(function() { return null; });
+  //chart
+    //.selectAll('.x.axis .tick')
+    //.text(function() { return null; });
 
   chart
     .selectAll('bar')
     .data(games)
     .enter()
       .append('rect')
-      .attr('class', 'bar')
+      .attr('class', function(d) {
+        //console.log(d.details.margin < 0);
+        return d.details.margin < 0 ? 'bar negative' : 'bar positive';
+      })
       .attr('x', function(d, i) {
         return xScale(i);
       })
       .attr('y', function(d) {
-        return yScale(d.details.home.pts - d.details.away.pts);
+        return height - yScale(Math.max(0, d.details.margin));
       })
       .attr('height', function(d) {
-        return height - yScale(d.details.home.pts
-          - d.details.away.pts);
+        return Math.abs(yScale(d.details.margin) - yScale(0));
       })
       .attr('width', xScale.rangeBand());
 
+//  chart
+//    .append('g')
+//    .attr('class', 'y axis')
+//    .attr('transform', 'translate(0,' + height + ')')
+//    .call(yAxis);
+
+  chart
+    .append('g')
+    .attr('class', 'x axis')
+    .append('line')
+    .attr('y1', height - yScale(0))
+    .attr('y2', height - yScale(0))
+    .attr('x2', width)
 
 }); // end getGames();
 
 
-
-
+ /*
+  * Returns stats for all season's games for the given team
+  *
+  */
 function getGames(team, callback) {
-// returns stats for all season's games for the given team
   d3.json('../data/box-scores-all.json', function(error, data) {
     
     var games = [];
 
-    // find all games involving the team
+    // find all games involving the team at hand
     d3.keys(data).forEach(function(d) {
+
       // if our given team is in this object, 
-      // include in games[] and incrent wins or losses
+      // include it in games[] 
       if (data[d].details.away.id == team ||
         data[d].details.home.id == team) {
         
@@ -129,5 +141,6 @@ function getGames(team, callback) {
       });
 
     callback(games);
-  }); // end d3.json()
+
+  }); // end d3.json
 } // end getGames
